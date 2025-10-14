2f3935622f91123c1f03c0107d9b358bd55eefef
udehlee <ritzeepita@gmail.com>
2025-10-14 22:08:58 +0100

    implement RAG generation logic with data fetching and summary

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/Udehlee/marketMind-ai/internals/adapters/llm"
+	"github.com/Udehlee/marketMind-ai/internals/adapters/market"
+	"github.com/Udehlee/marketMind-ai/internals/adapters/news"
+	"github.com/Udehlee/marketMind-ai/internals/core/domain"
+	"github.com/Udehlee/marketMind-ai/internals/core/service"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("find out about stock news today: ")
+	input, _ := reader.ReadString('\n')
+
+	news := news.NewRssAdapter()
+	market := market.NewMarketAdapter(input)
+	llm := llm.NewOpenai()
+
+	rag := service.NewRAG([]domain.DataSource{news, market}, llm)
+
+	answer, err := rag.Generate(context.Background())
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("\n", answer)
+
+}
