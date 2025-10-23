2eb7a93d101fa2a46aa5b6e38595967fcd16aee5
udehlee <ritzeepita@gmail.com>
2025-10-23 23:15:50 +0100

    refactor Generate to continue as long as one data source succeeds

--- internals/core/service/rag_service.go
+++ internals/core/service/rag_service.go
@@ -23,13 +23,20 @@ func NewRAG(d []domain.DataSource, llm *llm.Openai) *RAG {
 // Generate generates summmary
 func (r *RAG) Generate(ctx context.Context) (string, error) {
 	var allData []domain.ContentItem
+	success := false
 
 	for _, s := range r.datasource {
 		data, err := s.Fetch()
 		if err != nil {
-			return "", fmt.Errorf("failed to fetch data from source: %w", err)
+			fmt.Printf("failed to fetch from %T: %v\n", s, err)
+			continue
 		}
 		allData = append(allData, data...)
+		success = true
+	}
+
+	if !success {
+		return "", fmt.Errorf("failed to fetch data from all sources")
 	}
 
 	summary, err := r.llm.Summary(allData)
